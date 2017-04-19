ee29b5fcd9dc63ed036254678103a65639b4b55d
Alex Kale <alex.kale@prnewswire.com>
2017-04-19 12:27:29 -0400

    lesson 32 notes

--- 32-transition-hooks/content/js/components/contact/contact.component.js
+++ 32-transition-hooks/content/js/components/contact/contact.component.js
@@ -8,16 +8,20 @@ var contact = {
 			<div class="contact-details" ui-view="details"></div>
 		</div>
 	`,
+    // $transitions is component level
 	controller: function ($transitions) {
+        // returns boolean, runs when exiting a state, e.g. if navigating away from form
 		this.uiCanExit = function () {
 			console.log('Exiting...');
 			return window.confirm('Are you sure you want to leave?');
 		};
 		var myTransition = $transitions.onStart({
+            // if empty, will apply to all routes of that state, to customize use to: 'contact.*' for all sub components
 			to: 'contact.*'
 		}, function ($transition$) {
 			// $transition$
 		});
+        // needs to be manually destroyed
 		this.$onDestroy = function () {
 			myTransition();
 		};
@@ -31,15 +35,18 @@ angular
 		$stateProvider
 			.state('contact', {
 				url: '/contact',
+                // for use in transition hook checking which states require authorization
 				data: {
 					requiredAuth: true
 				},
+                // hooks
 				onEnter: function () {
 					console.log('onEnter');
 				},
 				onExit: function () {
 					console.log('onExit');
 				},
+                // still navigating in nested state
 				onRetain: function () {
 					console.log('onRetain');
 				},
--- 32-transition-hooks/content/js/components/home/home.component.js
+++ 32-transition-hooks/content/js/components/home/home.component.js
@@ -8,8 +8,11 @@ angular
 	.module('home')
 	.component('home', home)
 	.config(function ($stateProvider, $urlRouterProvider, $transitionsProvider) {
-		$transitionsProvider.onStart({
+        // .onStart method - first parameter is an object, then function
+		$transitionsProvider.onStart
+            // capture states
 			to: function (state) {
+                // if has state requires authorization - has data property and requiredAuth - !! converts to boolean with true value
 				return !!(state.data && state.data.requiredAuth);
 			}
 		}, function ($transition$) {
