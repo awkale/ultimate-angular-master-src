e9157b0c4a20500770dc62025e0f570b5bcdba97
Alex Kale <alex.kale@prnewswire.com>
2017-04-19 17:05:29 -0400

    lesson 35 notes

--- 35-resource/content/js/services/TodoService.js
+++ 35-resource/content/js/services/TodoService.js
@@ -16,6 +16,7 @@ function TodoService($resource) {
 	*/
 
 	return $resource('http://jsonplaceholder.typicode.com/todos/:id', {
+        // to have $resource to extract dynamic id property from individual object that was passed in
 		id: '@id'
 	}, {
 		update: {
