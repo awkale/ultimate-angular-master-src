3bfad73117c369ef1ba15b1087e9701ebbba5d10
Alex Kale <alex.kale@prnewswire.com>
2017-05-03 16:28:15 -0400

    lesson 36 notes

--- 36-promises/content/js/services/DemoService.js
+++ 36-promises/content/js/services/DemoService.js
@@ -4,9 +4,13 @@ function DemoService($q, $http) {
 	var comments = 'http://jsonplaceholder.typicode.com/comments';
 
 	this.getTodos = function () {
+        // promise object
 		var defer = $q.defer();
+        // gives a new instance XMLHttpRequest
 		var xhr = new XMLHttpRequest();
+        // gets called when XMLHttpRequest changes
 		xhr.onreadystatechange = function () {
+
 			if (xhr.readyState === 4) {
 				if (xhr.status === 200) {
 					defer.resolve(JSON.parse(xhr.responseText));
@@ -15,12 +19,15 @@ function DemoService($q, $http) {
 				}
 			}
 		};
+
 		xhr.open('GET', todos, true);
 		xhr.send();
 		return defer.promise;
 	};
 
+    // another way with constructor function - better
 	this.getComments = function () {
+        // instead of defer, return $q function with parameters resolve and reject
 		return $q(function (resolve, reject) {
 			var xhr = new XMLHttpRequest();
 			xhr.onreadystatechange = function () {
@@ -37,12 +44,15 @@ function DemoService($q, $http) {
 		});
 	};
 
+    // not creating your own promise
 	this.getAll = function () {
 		var promiseOne = $http.get(todos);
 		var promiseTwo = $http.get(comments);
+        // array of promises in $q function
 		return $q.all([promiseOne, promiseTwo]);
 	};
 
+    // new feature .race
 	this.getFirstResolved = function () {
 		var promiseOne = $http.get(todos);
 		var promiseTwo = $http.get(comments);
