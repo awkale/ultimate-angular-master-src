117f0db6a52ddec53f43d81119bd08eaee93215f
Alex Kale <alex.kale@prnewswire.com>
2017-05-03 17:28:40 -0400

    lesson 39 notes

--- 39-testing-controllers/content/tests/TodoController.spec.js
+++ 39-testing-controllers/content/tests/TodoController.spec.js
@@ -1,9 +1,13 @@
 describe('TodoController', function () {
+    // $controller fetches controller service
 	var $controller, TodoController;
 
+    // runs before each test; module instantiates app
 	beforeEach(module('app'));
 
+    // $injector allows to fetch dependencies for app
 	beforeEach(inject(function ($injector) {
+		// fetch $controller service to instantiate an instance of todo controller
 		$controller = $injector.get('$controller');
 
 		var $scope = {};
@@ -11,8 +15,10 @@ describe('TodoController', function () {
 		// mock the todo service
 		var TodoService = function () {};
 
+		// extend TodoService and create properties on prototype
 		TodoService.prototype.$save = function () {};
 
+		// part of resource object
 		TodoService.query = function () {
 			return {
 				$promise: { // mock the promise object $resource gives us
@@ -33,15 +39,18 @@ describe('TodoController', function () {
 			return true;
 		};
 
+		// instantiate controller
 		TodoController = $controller('TodoController as todo', {
 			$scope: $scope,
 			TodoService: TodoService
 		});
 	}));
 
+	// first test
 	it('should have get items from the service', function () {
 		var resp = TodoController.getTodos();
 
+		// test first item above to equal below
 		expect(TodoController.list[0]).toEqual({
 			id: 1,
 			title: 'Example Todo',
@@ -50,8 +59,10 @@ describe('TodoController', function () {
 	});
 
 	it('should delete the item from the service', function () {
+		// delete first 1
 		TodoController.deleteTodo(1);
 
+		// expect list array is empty
 		expect(TodoController.list.length).toEqual(0);
 	});
 
@@ -60,6 +71,7 @@ describe('TodoController', function () {
 
 		TodoController.newTodo();
 
+		// expect list array has 1
 		expect(TodoController.list.length).toEqual(1);
 	});
 });
