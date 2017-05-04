3e84794f3fceab14326ddf7bf2d6cea8898f268f
Alex Kale <alex.kale@prnewswire.com>
2017-05-04 16:24:00 -0400

    lesson 40 notes

--- 40-testing-directive-controllers/content/tests/Counter.spec.js
+++ 40-testing-directive-controllers/content/tests/Counter.spec.js
@@ -1,15 +1,21 @@
 describe('Counter', function () {
+    // $compile
 	var $compile, $scope, $controller, element;
 
 	beforeEach(module('app'));
 
 	beforeEach(inject(function ($injector) {
+        // compile allows to instantiate an element and compile it to a live angular object
 		$compile = $injector.get('$compile');
 
+        //
 		var $rootScope = $injector.get('$rootScope');
 
+        // compile counter to an element
 		element = angular.element('<counter></counter>');
+        // pass element in to compile service; in order to compile it against a particular scope; angular does it internally so this is needed for testing
 		$compile(element)($rootScope.$new());
+
 
 		$controller = element.controller('counter');
 		$scope = element.isolateScope() || element.scope();
@@ -27,7 +33,7 @@ describe('Counter', function () {
 
 	it('should decrement the counter correctly', function () {
 		$controller.decrement();
-
+        // each test is a new instance and scope, so it's -1
 		expect($controller.count).toEqual(-1);
 	});
 });
