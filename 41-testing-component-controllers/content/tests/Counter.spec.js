4d340a82fa0669de49f195753ff5bcbee88bd9ea
Alex Kale <alex.kale@prnewswire.com>
2017-05-09 15:13:59 -0400

    lesson 41 notes

--- 41-testing-component-controllers/content/tests/Counter.spec.js
+++ 41-testing-component-controllers/content/tests/Counter.spec.js
@@ -4,9 +4,12 @@ describe('Counter', function () {
 	beforeEach(module('app'));
 
 	beforeEach(inject(function ($injector) {
+        // gives the service to test the controller on its own without needing the template
 		$componentController = $injector.get('$componentController');
+        // take the instance to make another instance, `counter` is component that you want
 		controller = $componentController('counter',
 			{ $scope: {}},
+            // local binding
 			{ initialCount: initialCount }
 		);
 	}));
@@ -16,7 +19,9 @@ describe('Counter', function () {
 	});
 
 	it('should initialize to the correct count', function() {
+        // start at zero
 		expect(controller.count).toEqual(0);
+        // call life cycle hook since there is no DOM with instantiated component
 		controller.$onInit();
 		expect(controller.count).toEqual(initialCount);
 	});
